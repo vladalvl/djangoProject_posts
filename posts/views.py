abe12e22d498e42b030f5daf2b8b566a71f791d1
vl.levkovets <vlada.levkovets@yandex.ru>
2022-08-09 17:04:00 +0300

    0

---
+++ posts/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+
+def home(request):
+    print(request)
+    return render(request, 'base.html')
+
+
+def post(request):
+    post_content = request.headers
+    return render(request, 'posts.html', {'content': post_content})
+
+
+def about(request):
+    return render(request, 'about.html', {'content': '<h1>About</h1>'})
