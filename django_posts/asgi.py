7137e09807c04856635ac3dec0eafa90fcdf8887
vl.levkovets <vlada.levkovets@yandex.ru>
2022-09-16 22:17:11 +0300

    20

--- django_posts/asgi.py
+++ django_posts/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for djangoProject_posts project.
+ASGI config for django_posts project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoProject_posts.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_posts.settings')
 
 application = get_asgi_application()
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoProject_posts.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_posts.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
